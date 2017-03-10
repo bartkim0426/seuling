1baad63dff2db9d7cc71aea34a1971e90e69cb37
bartkim0426 <bartkim0426@gmail.com>
2017-03-10 13:52:29 +0900

    init wpsblog/: start django project- wpsblog

---
+++ wpsblog/wpsblog/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from django.contrib import admin
+from django.http.response import HttpResponse
+
+def home(request):
+    return HttpResponse("hello world")
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'$', home),
+]
