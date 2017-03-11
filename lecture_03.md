024356e54b8192d09206ac388df4464686b83d65
bartkim0426 <bartkim0426@gmail.com>
2017-03-11 18:40:13 +0900

    update news view: add search function

--- lecture_03.md
+++ lecture_03.md
@@ -16,3 +16,15 @@
 
 	news_items_list = news_dict.get("news")
 	
+*왓챠에 search 기능 추가*		
+/?search=sth 을 추가하여 search 기능 붙여보기(방법이 많음)
+1. if문 사용하기		
+		
+    search =  request.GET.get('search') 
+    print(search)
+		
+
+=> GET 방식으로 'search'라는 리퀘스트를 request 변수로 설정,		(만약 search가 있다면) search를 프린트		
+=> 즉, request.GET 방식으로 보내진 'search' 쿼리를 get 하라는 명령어이다		
+
+ 
--- wpsblog/wpsblog/views.py
+++ wpsblog/wpsblog/views.py
@@ -15,31 +15,74 @@ def room(request, room_id):
             content_type="application/json",
             )
 
+
 def news(request):
-    search = request.GET.get('search') # title에 search가 포함되어있는가?
+    search =  request.GET.get('search') 
 
-    response = requests.get("https://watcha.net/home/news.json?page=1&per=50")
-   #  from IPython import embed; embed()
-    news_dict = json.loads(response.content) # news_dict =  response.json()
+    url = "https://watcha.net/home/news.json?page=1&per=50"
+    response = requests.get(url)
     
-    news_items_list = news_dict.get("news")
-    if search:
-        search = search
-    else: 
-        search = ""
-    content = "<h1>News</h1>" +\
-              "<p>This is new page. </p>" +\
-              "".join([
-                  "<h2>{title}</h2><img src={image_src} alt='news_image' /><p>{content}</p>".format(
-                title = news.get('title'),
-                image_src = news.get('image'),
-                content = news.get('content'),
-                      )
-                  for news
-                  in news_items_list
-                  ])
+    news_dict = json.loads(response.content)
+    news_list =  news_dict.get('news')
 
+    if search:
+        news_list = list(filter(
+            lambda news: search in news.get('content'), 
+            news_list,
+            ))
+
+    content = "<h1>NEWS by seul</h1>" +\
+            "<p>this is my news page</p>" +\
+            "<p>{count}개의 영화 뉴스 정보가 있습니다.</p>".format(count= len(news_list)) +\
+            "".join([
+                "<p><h2>{title}</h2></p><img src={image} alt='movie_img'/> <p>{content}</p>".format(
+                    title=news['title'], 
+                    image=news['image'],
+                    content=news['content'],)
+                for news in news_list
+                ])
     return HttpResponse(
-            content,
-            # content_type="application/json",
-            )
+           content
+
+           
+           
+#def news(request):
+#    search = request.GET.get('search') # title에 search가 포함되어있는가?
+#
+#    response = requests.get("https://watcha.net/home/news.json?page=1&per=50")
+#   #  from IPython import embed; embed()
+#    news_dict = json.loads(response.content) # news_dict =  response.json()
+#    
+#    news_items_list = news_dict.get("news")
+#    if search:
+#            news_items_list = list(filter(
+#                lambda news: search in news.get('content'),
+#                news_items_list, 
+#                ))
+#    else: 
+#        search = ""
+#    
+#    content = "<h1>News</h1>" +\
+#              "<p>This is new page. </p>" +\
+#              "<p>{count}개의 검색 결과가 있습니다</p>".format(count=len(news_items_list)) +\
+#              "".join([
+#                "<h2>{title}</h2><img src={image_src} alt='news_image' /><p>{content}</p>".format(
+#                title = news.get('title'),
+#                image_src = news.get('image'),
+#                content = news.get('content'),
+#                      ) 
+#                # +\
+#                #              "<p>search {search}:{news_search}</p>".format(news_search=
+#                #  [x for x in news.get('content').split(" ")
+#                #      if search in x], 
+#                #  search = search
+#                #  )
+#                  for news
+#                  in news_items_list
+#                  if search in news.get('content')
+#                  ])
+#
+#    return HttpResponse(
+#            content,
+#            # content_type="application/json",
+#            )
