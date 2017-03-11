d73aa93f34fa3bb672250835c0f1512e69f7ac71
bartkim0426 <bartkim0426@gmail.com>
2017-03-12 01:05:06 +0900

    update renderer: add header, footer func

--- wpsblog/wpsblog/renderer.py
+++ wpsblog/wpsblog/renderer.py
@@ -2,8 +2,13 @@ from django.http import HttpResponse
 from django.conf import settings
 
 def render(template_name, context):
+    header_content = open(settings.BASE_DIR + "/templates/header.html", "r").read()
+    footer_content = open(settings.BASE_DIR + "/templates/footer.html", "r").read()
+        
     with open(settings.BASE_DIR + "/templates/"+ template_name + ".html", "r") as template:
         content = template.read()
+        content = content.replace("## header ##", header_content)
+        content = content.replace("## footer ##", footer_content)
         for key, value in context.items():
             content = content.replace(
                    "## " + key + " ##", value 
