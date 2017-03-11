4b21821695b0dffaf27cfc77e03a8c3d4a3b46e9
bartkim0426 <bartkim0426@gmail.com>
2017-03-12 00:55:58 +0900

    init renderer: make render func making content

---
+++ wpsblog/wpsblog/renderer.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.conf import settings
+
+def render(template_name, context):
+    with open(settings.BASE_DIR + "/templates/"+ template_name + ".html", "r") as template:
+        content = template.read()
+        for key, value in context.items():
+            content = content.replace(
+                   "## " + key + " ##", value 
+                    )
+    return HttpResponse(content)
--- wpsblog/wpsblog/views.py
+++ wpsblog/wpsblog/views.py
@@ -2,14 +2,10 @@ import requests
 import json
 
 from django.http import HttpResponse  
-from django.conf import settings
+from wpsblog.renderer import render
 
 def home(request):
-    with open(settings.BASE_DIR + "/templates/home.html", "r") as template:
-        content = template.read()
-        site_name = "seulchan's blog"
-        content = content.replace("## site_name ##", site_name)
-    return HttpResponse(content)
+    return render("home", {"site_name":"seul's blog"})
 
 def room(request, room_id):
    # 방 번호 (room_id) 직방의 데이터를 그대로 보여주는 뷰 (컨트롤러)
@@ -36,19 +32,17 @@ def news(request):
             lambda news: search in news.get('content'), 
             news_list,
             ))
-    
-    with open(settings.BASE_DIR+"/templates/news.html", "r") as template:
-        content = template.read()
-
-        count = len(news_list)
-        news_content = "".join([
-                    "<p><h2>{title}</h2></p><img src={image} alt='movie_img'/> <p>{content}</p>".format(
-                        title=news['title'], 
-                        image=news['image'],
-                        content=news['content'],)
-                    for news 
-                    in news_list
-                    ])
-        content = content.replace("## count ##", str(count))
-        content = content.replace("## news_content ##", news_content)
-        return HttpResponse(content)
+    count = len(news_list)
+    news_content = "".join([
+        "<h2>{title}</h2><img src={img_src} alt='movie_image'/><p>{content}</p>".format(
+            title = news.get('title'),
+            img_src = news.get('image'),
+            content = news.get('content'),
+            )
+        for news
+        in news_list
+        ])
+    return render("news", {
+        "count": str(count),
+        "news_content": news_content
+        }) 
