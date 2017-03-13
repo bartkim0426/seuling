146e7cac9c8fb311580f1020ff896e6f1e8efc3b
bartkim0426 <bartkim0426@gmail.com>
2017-03-14 01:01:36 +0900

    refactor policy: using include

--- wpsblog/wpsblog/urls.py
+++ wpsblog/wpsblog/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from wpsblog.views import *
 
@@ -9,19 +9,11 @@ urlpatterns = [
     url(r'^rooms/(?P<room_id>\d+)/$', room, name="room"),
     url(r'^news/$', news, name="news"),
     url(r'^about/us/$', about, name="about"),
-    url(r'^policy/terms/$', terms, name="terms"),
-    url(r'^policy/privacy/$', privacy, name="privacy"),
-    url(r'^policy/disclaimer/$', disclaimer, name='disclaimer'),
-    
+
+    url(r'^policy/', include([
+        url(r'^terms/$', terms, name="terms"),
+        url(r'^privacy/$', privacy, name="privacy"),
+        url(r'^disclaimer/$', disclaimer, name='disclaimer'),
+        ], namespace='policy'))
 ]
-
-
-# 1. about page (.../about , .../about/us , .../about-us/)
-
-# 2. 정책
-# 이용약관 page ( /terms/ )
-# 개인정보 취급방침 페이지 (... /privacy/)
-# 법적 고지와 책임의 한계 (... /disclaimer/)
-
-# 이것들을 header에 넣어서 동작하게
 
