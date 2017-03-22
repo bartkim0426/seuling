aed98e8f8075ca3c59759646c1dc9f68b1698214
Seul Chan Kim <seulchankim@Seului-MacBook-Pro.local>
2017-03-22 10:01:46 +0900

    Update naver_posts_list: add search func

--- wpsblog/wpsblog/views/naver_posts/list.py
+++ wpsblog/wpsblog/views/naver_posts/list.py
@@ -5,10 +5,13 @@ from wpsblog.models import Crawlnaver
 
 def naver_posts_list(request):
     keyword = request.GET.get('keyword')
+    search = request.GET.get('search')
+
     naver_posts = Crawlnaver.objects.all()
     
     if keyword:
         naver_posts = naver_posts.filter(keyword=keyword)
+
 #     if keyword:
 #         naver_posts = [
 #                 naver_post
@@ -16,12 +19,14 @@ def naver_posts_list(request):
 #                 in naver_posts
 #                 if naver_post.keyword == keyword
 #                 ]
-
+    if search:
+        naver_posts = naver_posts.filter(content__icontains=search)
     return render(
            request,
            "naver_posts/list.html",
            {
                "naver_posts": naver_posts,
                "keyword": keyword,
+               "search": search,
             },
     )
