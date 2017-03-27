3f5ca2bbeaae55be176a803701d4ded6336dd52f
bartkim0426 <seulchankim@Seului-MacBook-Pro.local>
2017-03-28 02:30:41 +0900

    Init PostManager: using objects.public

--- wpsblog/wpsblog/models/post.py
+++ wpsblog/wpsblog/models/post.py
@@ -1,12 +1,23 @@
 from django.db import models
 from django.urls.base import reverse
 
+class PostManager(models.Model):
+
+    def public(self):
+        return self.filter(is_public=True)
+
 
 class Post(models.Model):
+
+    objects = PostManager()
+
     title = models.CharField(
         max_length=120,
     )
     content = models.TextField()
+    is_public = models.BooleanField(
+            default=True
+            )
     
     def __str__(self):
         return self.title
