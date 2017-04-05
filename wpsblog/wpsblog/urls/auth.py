45ccb05ddb2b6f7d1e707cfa6a68343a2ed044ea
bartkim0426 <seulchankim@Seului-MacBook-Pro.local>
2017-04-06 03:14:53 +0900

    Refactor login: using LoginRequiredMixin

--- wpsblog/wpsblog/urls/auth.py
+++ wpsblog/wpsblog/urls/auth.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url
-from wpsblog.views.auth import LoginView, SignupView, LogoutView, mypage
+from wpsblog.views.auth import LoginView, SignupView, LogoutView, MyPageView 
 
 
 urlpatterns = [
         url(r'^login/$', LoginView.as_view(), name='login'),
         url(r'^signup/$', SignupView.as_view(), name='signup'),
         url(r'^logout/$', LogoutView.as_view(), name='logout'),
-        url(r'^my/page/$', mypage, name="mypage"),
+        url(r'^my/page/$', MyPageView.as_view(), name="mypage"),
         ]
--- wpsblog/wpsblog/views/auth/mypage.py
+++ wpsblog/wpsblog/views/auth/mypage.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
+# from django.contrib.auth.decorators import login_required
+from django.views.generic import View 
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-@login_required
-def mypage(request):
+class MyPageView(LoginRequiredMixin, View):
 
-    return render(
-            request,
-            "auth/mypage.html",
-            {},
-            )
+    def get(self, request, *args, **kwargs):
+        return render(
+                request,
+                "auth/mypage.html",
+                {},
+                )
+# @login_required
+# def mypage(request):
+# 
+#     return render(
+#             request,
+#             "auth/mypage.html",
+#             {},
+#             )
