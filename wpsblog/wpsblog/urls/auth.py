4a0814087a8d21c40d25beca44b2d7119dc5e649
bartkim0426 <seulchankim@Seului-MacBook-Pro.local>
2017-04-06 03:04:07 +0900

    Refactor login, logout by CBV

--- wpsblog/wpsblog/urls/auth.py
+++ wpsblog/wpsblog/urls/auth.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url
-from wpsblog.views.auth import login, signup, logout, mypage
+from wpsblog.views.auth import LoginView, SignupView, LogoutView, mypage
 
 
 urlpatterns = [
-        url(r'^login/$', login, name='login'),
-        url(r'^signup/$', signup, name='signup'),
-        url(r'^logout/$', logout, name='logout'),
+        url(r'^login/$', LoginView.as_view(), name='login'),
+        url(r'^signup/$', SignupView.as_view(), name='signup'),
+        url(r'^logout/$', LogoutView.as_view(), name='logout'),
         url(r'^my/page/$', mypage, name="mypage"),
         ]
