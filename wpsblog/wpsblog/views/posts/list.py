1de533521497a1d9fc6bd7f6aa0a0c4c520b1d59
bartkim0426 <seulchankim@Seului-MacBook-Pro.local>
2017-04-06 02:27:32 +0900

    Refactor pagination: using django.core.paginator

--- wpsblog/wpsblog/views/posts/list.py
+++ wpsblog/wpsblog/views/posts/list.py
@@ -2,19 +2,25 @@ from django.shortcuts import render
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from wpsblog.models import Post, Comment
 
+
 def list(request):
 
-    posts_list = Post.objects.public()
-    p = Paginator(posts_list, 5)
+    page = request.GET.get("page", 1)
+    per = request.GET.get("per", 3)
 
-    page = request.GET.get('page')
-
-    try:
-        posts = p.page(page)
-    except PageNotAnInteger:
-        posts = p.page(1)
-    except EmptyPage:
-        posts = p.page(p.num_pages)
+    paginator = Paginator(Post.objects.public(), per)
+    
+    posts = paginator.page(page)
+#     p = Paginator(posts_list, 5)
+# 
+#     page = request.GET.get('page')
+# 
+#     try:
+#         posts = p.page(page)
+#     except PageNotAnInteger:
+#         posts = p.page(1)
+#     except EmptyPage:
+#         posts = p.page(p.num_pages)
 
     return render(
            request,
