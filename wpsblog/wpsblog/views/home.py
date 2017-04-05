7c581208f80ba21fdb02d06c553c8a118cdc63c3
bartkim0426 <seulchankim@Seului-MacBook-Pro.local>
2017-04-06 02:41:34 +0900

    Refactoring Home: using CBV

--- wpsblog/wpsblog/views/home.py
+++ wpsblog/wpsblog/views/home.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import View
 
 
-def home(request):
-    return render(
-           request,
-           "home.html",
-           {},
-           )
+# def home(request):
+#     return render(
+#            request,
+#            "home.html",
+#            {},
+#            )
+class HomeView(View):
+    
+    def get(self, request, *args, **kwargs):
+        return render(
+            request,
+            "home.html",
+            {},
+        )
