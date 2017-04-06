88043befa726527f615aba64f6d12f6a5befdb83
bartkim0426 <seulchankim@Seului-MacBook-Pro.local>
2017-04-07 01:11:48 +0900

    Refactor mypage: using TemplateView

--- wpsblog/wpsblog/views/auth/mypage.py
+++ wpsblog/wpsblog/views/auth/mypage.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render
 # from django.contrib.auth.decorators import login_required
-from django.views.generic import View 
+from django.views.generic import View, TemplateView 
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class MyPageView(LoginRequiredMixin, View):
+class MyPageView(LoginRequiredMixin, TemplateView):
+    template_name = "auth/mypage.html"
 
-    def get(self, request, *args, **kwargs):
-        return render(
-                request,
-                "auth/mypage.html",
-                {},
-                )
+# class MyPageView(LoginRequiredMixin, View):
+# 
+#     def get(self, request, *args, **kwargs):
+#         return render(
+#                 request,
+#                 "auth/mypage.html",
+#                 {},
+#                 )
 # @login_required
 # def mypage(request):
 # 
