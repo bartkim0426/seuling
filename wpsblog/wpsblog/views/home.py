e7e814bee58047dfe2d27ed18ffaec481374058b
bartkim0426 <seulchankim@Seului-MacBook-Pro.local>
2017-04-07 01:09:44 +0900

    Refactor home: using TemplateView

--- wpsblog/wpsblog/views/home.py
+++ wpsblog/wpsblog/views/home.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import View
+from django.views.generic import View, TemplateView
 
+
+class HomeView(TemplateView):
+    template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+
+        context["site_name"] = "seul blog"
+        return context
 
 # def home(request):
 #     return render(
@@ -8,11 +17,11 @@ from django.views.generic import View
 #            "home.html",
 #            {},
 #            )
-class HomeView(View):
-    
-    def get(self, request, *args, **kwargs):
-        return render(
-            request,
-            "home.html",
-            {},
-        )
+# class HomeView(View):
+#     
+#     def get(self, request, *args, **kwargs):
+#         return render(
+#             request,
+#             "home.html",
+#             {},
+#         )
