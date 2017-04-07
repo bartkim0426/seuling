42c2caa49c97380a56bfa0318c42f2b43c240089
bartkim0426 <seulchankim@Seului-MacBook-Pro.local>
2017-04-07 14:32:41 +0900

    Refactor list: using get_paginate_by

--- wpsblog/wpsblog/views/posts/list.py
+++ wpsblog/wpsblog/views/posts/list.py
@@ -9,11 +9,32 @@ class PostListView(PostBaseView, ListView):
 
     template_name = "posts/list.html"
     context_object_name = "posts"
+    
+    def get_paginate_by(self, queryset):
+        return self.request.GET.get("per", 3)
 
-    def get_queryset(self):
-        queryset = super(PostListView, self).get_queryset()
-        return queryset.filter(is_public=True)
+#     def get_queryset(self):
+#         queryset = super(PostListView, self).get_queryset()
+#         page = self.request.GET.get("page", 1)
+#         per = self.request.GET.get("per", 3)
+# 
+#         paginator = Paginator(queryset, per)
+#         queryset = paginator.page(page)
+# 
+#         return queryset
+# 
+#     def get_context_data(self, **kwargs):
+#         context = super(PostListView, self).get_context_data(**kwargs)
+#         paginator = Paginator(context["posts"], 3)
+#         context["posts"] = paginator.page(1)
+# 
+#         return context
 
+
+#     def get_queryset(self):
+#         queryset = super(PostListView, self).get_queryset()
+#         return queryset.filter(is_public=True)
+# 
 # def list(request):
 # 
 #     page = request.GET.get("page", 1)
